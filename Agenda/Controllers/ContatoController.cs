a6915efc6012b1874fd0777bd5206cf5590de083
Moacir <moacir9401@gmail.com>
2022-09-18 19:18:15 -0300

    criando tela de contato

---
+++ Agenda/Controllers/ContatoController.cs
@@ -0,0 +1,94 @@
+﻿using Agenda.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly Context _context;
+
+        public ContatoController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Contato")]
+        public async Task<ActionResult> Index()
+        {
+            var contatos = await _context.Contatos.ToListAsync();
+
+            return View(contatos);
+        }
+         
+        [HttpGet]
+        public async Task<ActionResult> VisualizarContato(long id)
+        {
+            var contato = await _context.Contatos.FirstAsync(u => u.Id == id);
+            return View(contato);
+        }
+
+        [HttpGet]
+        public ActionResult CriarContato()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public ActionResult CriarContato(Contato contato)
+        {
+            try
+            { 
+                _context.Add(contato);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EditarContato(int id)
+        {
+            var contato = await _context.Contatos.FirstAsync(u => u.Id == id);
+            return View(contato);
+        }
+         
+        [HttpPost]
+        public ActionResult EditarContato(int id, Contato contato)
+        {
+            try
+            {
+                _context.Contatos.Update(contato);                  
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+          
+         
+        [HttpGet]
+        public async Task<ActionResult> ExcluirContato(int id)
+        {
+            try
+            {
+                var contato = await _context.Contatos.FirstAsync(u => u.Id == id);
+                _context.Contatos.Remove(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Agenda/Models/Contato.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient.Server;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda.Models
+{ 
+    public class Contato
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Nome { get; set; } 
+        [Required(ErrorMessage = "Email é obrigatório")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefone é obrigatório")]
+        [Display(Name = "Telefone")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(15)]
+        [DisplayFormat(DataFormatString = "{0:(##) #####-####}", ApplyFormatInEditMode = true)]
+        public string Telefone { get; set; }
+    }
+
+}
